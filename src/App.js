d54e8b1235e895be00321c0ae63d7d411db38768
Mohamed Ahmed <a.mohamed.cali@gmail.com>
2020-05-21 00:33:39 +0200

    Build movie component

--- src/App.js
+++ src/App.js
@@ -1,11 +1,12 @@
 import React, { Component } from 'react';
+import Movies from './components/movies';
 import './App.css';
 
 class App extends Component {
 	render() {
 		return (
 			<div className="container">
-				<h1>Hello World</h1>
+				<Movies />
 			</div>
 		);
 	}
