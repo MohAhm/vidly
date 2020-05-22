3a2f77c2fc16e57b86281057bed2e7f76ab4983e
Mohamed Ahmed <a.mohamed.cali@gmail.com>
2020-05-22 12:40:06 +0200

    feat: Add like component

--- src/components/movies.js
+++ src/components/movies.js
@@ -1,5 +1,6 @@
 import React, { Component } from 'react';
 import { getMovies } from '../services/fakeMovieService';
+import Like from './common/like';
 
 
 class Movies extends Component {
@@ -11,6 +12,18 @@ class Movies extends Component {
         const movies = this.state.movies.filter(m => m._id !== movie._id);
 
         this.setState({ movies })
+    };
+
+    handleLike = movie => {
+        const movies = [...this.state.movies];
+        const index = movies.indexOf(movie);
+
+        movies[index] = { ...movies[index] };
+        movies[index].liked = !movies[index].liked;
+
+        this.setState({ movies });
+
+        // console.log('Like Clicked', movie);
     };
 
     render() {
@@ -30,7 +43,8 @@ class Movies extends Component {
                             <th>Genre</th>
                             <th>Stock</th>
                             <th>Rate</th>
-                            <th></th>
+                            <th />
+                            <th />
                         </tr>
                     </thead>
                     <tbody>
@@ -40,6 +54,12 @@ class Movies extends Component {
                                 <td>{movie.genre.name}</td>
                                 <td>{movie.numberInStock}</td>
                                 <td>{movie.dailyRentalRate}</td>
+                                <td>
+                                    <Like
+                                        liked={movie.liked}
+                                        onClick={() => this.handleLike(movie)}
+                                    />
+                                </td>
                                 <td>
                                     <button onClick={() => this.handleDelete(movie)} className="btn btn-danger btn-sm">
                                         Delete
