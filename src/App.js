eb4bc7341be31bd43024de03fea100d48497311a
Mohamed Ahmed <a.mohamed.cali@gmail.com>
2020-05-25 22:28:22 +0200

    Add React Router

--- src/App.js
+++ src/App.js
@@ -1,13 +1,31 @@
 import React, { Component } from 'react';
+import { Route, Redirect, Switch } from 'react-router-dom';
 import Movies from './components/movies';
+import Customers from './components/customers';
+import Rentals from './components/rentals';
+import NotFound from './components/notFound';
 import './App.css';
+import NavBar from './components/navbar';
+import MovieForm from './components/movieForm';
+
 
 class App extends Component {
 	render() {
 		return (
-			<div className="container">
-				<Movies />
-			</div>
+			<>
+				<NavBar />
+				<div className="container">
+					<Switch>
+						<Route path="/movies/:id" component={MovieForm}/>
+						<Route path="/movies" component={Movies}></Route>
+						<Route path="/customers" component={Customers}></Route>
+						<Route path="/rentals" component={Rentals}></Route>
+						<Route path="/not-found" component={NotFound}></Route>
+						<Redirect from="/" exact to="/movies" />
+						<Redirect to="/not-found" />
+					</Switch>
+				</div>
+			</>
 		);
 	}
 }
