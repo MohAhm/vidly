e26ea6032f4cf26d13820360002569c0aef64a69
Mohamed Ahmed <a.mohamed.cali@gmail.com>
2020-05-26 15:07:30 +0200

    feat: Add login form and validation

--- src/App.js
+++ src/App.js
@@ -4,9 +4,10 @@ import Movies from './components/movies';
 import Customers from './components/customers';
 import Rentals from './components/rentals';
 import NotFound from './components/notFound';
-import './App.css';
 import NavBar from './components/navbar';
 import MovieForm from './components/movieForm';
+import LoginForm from './components/loginForm';
+import './App.css';
 
 
 class App extends Component {
@@ -16,6 +17,7 @@ class App extends Component {
 				<NavBar />
 				<div className="container">
 					<Switch>
+						<Route path="/login" component={LoginForm}/>
 						<Route path="/movies/:id" component={MovieForm}/>
 						<Route path="/movies" component={Movies}></Route>
 						<Route path="/customers" component={Customers}></Route>
